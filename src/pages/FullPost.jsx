65ab6158eedf8a1e52b4676a056f753cc5197463
BLVCK7 <101505525+BLVCK7@users.noreply.github.com>
2022-07-04 14:11:27 +0300

    fix URI

--- src/pages/FullPost.jsx
+++ src/pages/FullPost.jsx
@@ -14,7 +14,7 @@ export const FullPost = () => {
 
   React.useEffect(() => {
     axios
-      .get(`${process.env.REACT_APP_API_URL}${id}`)
+      .get(`${process.env.REACT_APP_API_URL}/${id}`)
       .then((res) => {
         setData(res.data);
         setLoading(false);
@@ -35,7 +35,7 @@ export const FullPost = () => {
       <Post
         id={data._id}
         title={data.title}
-        imageUrl={data.imageUrl ? `${process.env.REACT_APP_API_URL}${data.imageUrl}` : ''}
+        imageUrl={data.imageUrl ? `${process.env.REACT_APP_API_URL}/${data.imageUrl}` : ''}
         // imageUrl="https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png"
         user={data.user}
         createdAt={data.createdAt}
