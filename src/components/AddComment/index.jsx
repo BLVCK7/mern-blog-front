38db687453089d4943bc4626a4d2ca076bb4e326
BLVCK7 <101505525+BLVCK7@users.noreply.github.com>
2022-07-05 21:05:29 +0300

    сделал отправку комментария на сервер. Остановился на гет запросе и визуальном добавлении коммента в блок

--- src/components/AddComment/index.jsx
+++ src/components/AddComment/index.jsx
@@ -1,12 +1,39 @@
-import React from "react";
+import React from 'react';
 
-import styles from "./AddComment.module.scss";
+import styles from './AddComment.module.scss';
 
-import TextField from "@mui/material/TextField";
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
+import TextField from '@mui/material/TextField';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+
+import { useParams } from 'react-router-dom';
+
+import axios from '../../axios';
 
 export const Index = () => {
+  const [comments, setComments] = React.useState('');
+  const { id } = useParams();
+
+  const onSubmit = async (e) => {
+    try {
+      const fields = {
+        comments,
+        id,
+      };
+
+      await axios.post(`http://localhost:4444/comment/add`, fields);
+
+      setComments('');
+
+      // const _id = isEditing ? id : data._id;
+
+      // navigate(`/posts/${_id}`);
+    } catch (error) {
+      console.warn(error);
+      alert('Ошибка при добавлении комментария');
+    }
+  };
+
   return (
     <>
       <div className={styles.root}>
@@ -18,11 +45,15 @@ export const Index = () => {
           <TextField
             label="Написать комментарий"
             variant="outlined"
+            value={comments}
+            onChange={(e) => setComments(e.target.value)}
             maxRows={10}
             multiline
             fullWidth
           />
-          <Button variant="contained">Отправить</Button>
+          <Button onClick={onSubmit} variant="contained">
+            Отправить
+          </Button>
         </div>
       </div>
     </>
--- src/components/CommentsBlock.jsx
+++ src/components/CommentsBlock.jsx
@@ -1,45 +1,68 @@
-import React from "react";
+import React from 'react';
 
-import { SideBlock } from "./SideBlock";
-import ListItem from "@mui/material/ListItem";
-import ListItemAvatar from "@mui/material/ListItemAvatar";
-import Avatar from "@mui/material/Avatar";
-import ListItemText from "@mui/material/ListItemText";
-import Divider from "@mui/material/Divider";
-import List from "@mui/material/List";
-import Skeleton from "@mui/material/Skeleton";
+import { SideBlock } from './SideBlock';
+import ListItem from '@mui/material/ListItem';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import ListItemText from '@mui/material/ListItemText';
+import Divider from '@mui/material/Divider';
+import List from '@mui/material/List';
+import Skeleton from '@mui/material/Skeleton';
+import { useSelector } from 'react-redux';
+import { selectIsAuth } from '../redux/slices/auth';
+// import axios from 'axios';
+// import { useParams } from 'react-router-dom';
 
 export const CommentsBlock = ({ items, children, isLoading = true }) => {
+  const isAuth = useSelector(selectIsAuth);
+  // const [data, setData] = React.useState();
+  // const { id } = useParams();
+
+  // const _id = JSON.stringify(id);
+
+  // console.log(data, id, _id);
+
+  // React.useEffect((_id) => {
+  //   axios.get(`http://localhost:4444/comment/get`, _id).then((res) => {
+  //     setData(res.data);
+  //   });
+  // }, []);
+
   return (
-    <SideBlock title="Комментарии">
-      <List>
-        {(isLoading ? [...Array(5)] : items).map((obj, index) => (
-          <React.Fragment key={index}>
-            <ListItem alignItems="flex-start">
-              <ListItemAvatar>
-                {isLoading ? (
-                  <Skeleton variant="circular" width={40} height={40} />
-                ) : (
-                  <Avatar alt={obj.user.fullName} src={obj.user.avatarUrl} />
-                )}
-              </ListItemAvatar>
-              {isLoading ? (
-                <div style={{ display: "flex", flexDirection: "column" }}>
-                  <Skeleton variant="text" height={25} width={120} />
-                  <Skeleton variant="text" height={18} width={230} />
-                </div>
-              ) : (
-                <ListItemText
-                  primary={obj.user.fullName}
-                  secondary={obj.text}
-                />
-              )}
-            </ListItem>
-            <Divider variant="inset" component="li" />
-          </React.Fragment>
-        ))}
-      </List>
-      {children}
-    </SideBlock>
+    <>
+      {isAuth ? (
+        <SideBlock title="Комментарии">
+          <List>
+            {(isLoading ? [...Array(5)] : items).map((obj, index) => (
+              <React.Fragment key={index}>
+                <ListItem alignItems="flex-start">
+                  <ListItemAvatar>
+                    {isLoading ? (
+                      <Skeleton variant="circular" width={40} height={40} />
+                    ) : (
+                      <Avatar alt={obj.user.fullName} src={obj.user.avatarUrl} />
+                    )}
+                  </ListItemAvatar>
+                  {isLoading ? (
+                    <div style={{ display: 'flex', flexDirection: 'column' }}>
+                      <Skeleton variant="text" height={25} width={120} />
+                      <Skeleton variant="text" height={18} width={230} />
+                    </div>
+                  ) : (
+                    <ListItemText primary={obj.user.fullName} secondary={obj.text} />
+                  )}
+                </ListItem>
+                <Divider variant="inset" component="li" />
+              </React.Fragment>
+            ))}
+          </List>
+          {children}
+        </SideBlock>
+      ) : (
+        <div>
+          <p>Авторизуйтесь для просмотра комментариев</p>
+        </div>
+      )}
+    </>
   );
 };
